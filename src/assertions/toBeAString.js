2737fd5cd6552ea4bed9ddbfa43f536cb50863f7
Steve Saxton <steves@codeuniquely.co.uk>
2018-01-07 17:40:03 +0000

    Removed Junk

--- src/assertions/toBeAString.js
+++ src/assertions/toBeAString.js
@@ -2,29 +2,27 @@
  * This source code is licensed under the MIT-style license found in the
  * LICENSE file in the root directory of this source tree. *
  *
- * @providesModule toBeAFunctionAssertion
+ * @providesModule toBeAStringAssertion
  * @flow
  */
 
 import type { EnzymeObject, Matcher } from '../types';
-import html from '../utils/html';
 import getNodeName from '../utils/name';
-import single from '../utils/single';
 
-function toBeAFunction (enzymeWrapper: EnzymeObject): Matcher {
-  const pass = (typeof enzymeWrapper === 'function');
+function toBeAString (enzymeWrapper: EnzymeObject): Matcher {
+  const pass = (typeof enzymeWrapper === 'string');
   return {
     pass,
     message: `Expected (${getNodeName(
       enzymeWrapper
-    )}) to be a "Function" but it wasn't.`,
+    )}) to be a "String" but it wasn't.`,
     negatedMessage: `Expected (${getNodeName(
       enzymeWrapper
-    )}) not to be a "Function" but it was`,
+    )}) not to be a "String" but it was`,
     contextualInformation: {
       expected: ` ${getNodeName(enzymeWrapper)}`,
     },
   };
 }
 
-export default toBeAFunction;
+export default toBeAString;
